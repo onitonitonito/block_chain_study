306d4cc48f91dcb1e769c835746e495bd51c14df
What Onito3 on Earth! <nitt0x0@gmail.com>
2018-09-30 23:22:15 +0900

    2.22-neat Elliptic

--- _test/_test_ecliptic_curve.py
+++ _test/_test_ecliptic_curve.py
@@ -5,6 +5,7 @@
 """
 # print(__doc__)
 
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -19,63 +20,65 @@ def plot_elliptic(a=-1, b=1):
     )
     return _plot
 
-
-def main(a=-1, b=1):
-    plot_elliptic(a, b)
+def show_decorated(a, b, title=1, xlabel=1):
     plt.grid()
-    plt.title("\
-        elliptic Curves Depending on a, b\n\
-        Y**2 = X**3 + X * a + b")
-    plt.xlabel("\
-        a={0:}, b={1:},\
-        Y**2 = X**3 + X * ({0:}) + ({1:})".format(a, b))
-    plt.show()
 
+    if title:
+        plt.title("\
+            elliptic Curves Depending on a, b\n\
+            Y**2 = X**3 + X * a + b")
+    if xlabel:
+        plt.xlabel("\
+            a={0:}, b={1:},\
+            Y**2 = X**3 + X * ({0:}) + ({1:})".format(a, b))
 
-if __name__ == '__main__':
+def show_some_curves():
+    plt.figure(1)
+
+    plt.subplot(211)
+    plot_elliptic(-1, 1)
+    show_decorated(-1, 1, title=0)
+
+    plt.subplot(212)
+    plot_elliptic(-5, 5)
+    show_decorated(-5, 5, title=0)
+
+def show_step_b_step_curves():
+    # change various a, b  check how graph changes
+    for _a in range(-5, 6, 1):
+        # _a = [-5 ~ 5] / _b = 1
+        plot_elliptic(_a, 1)
+        show_decorated(_a, 1, title=0)
+        plt.show()
+
+    for _b in range(-5, 6, 1):
+        # _a = -1 / _b = [-5 ~ 5]
+        plot_elliptic(-1, _b)
+        show_decorated(-1, _b, title=0)
+        plt.show()
+
+def show_various_curves_comparison():
     plt.figure(1)
 
     plt.subplot(211)
     for _a in range(-5, 6, 1):
         # _a = [-5 ~ 5] / _b = 0
         plot_elliptic(_a, 0)
-
-    plt.grid()
-    plt.xlabel("\
-        a=-5 ~ 5, b=0,\
-        Y**2 = X**3 + X * ({0:}) + ({1:})".format(_a, 0))
-
+    show_decorated(_a, 0, title=0)
 
     plt.subplot(212)
     for _b in range(-5, 6, 1):
         # _b = [-5 ~ 5] / _a = 0
         plot_elliptic(0, _b)
+    show_decorated(0, _b, title=0)
 
-    plt.grid()
-    plt.xlabel("\
-        a=0, b=-5 ~ 5,\
-        Y**2 = X**3 + X * ({0:}) + ({1:})".format(0, _b))
 
+
+if __name__ == '__main__':
+    show_step_b_step_curves()
+
+    show_some_curves()
     plt.show()
 
-
-
-    # # change various a, b  check how graph changes
-    # for _a in range(-5, 6, 1):
-    #     # _a = [-5 ~ 5] / _b = 1
-    #     main(_a)
-    #
-    # for _b in range(-5, 6, 1):
-    #     # _a = -1 / _b = [-5 ~ 5]
-    #     main(-1, _b)
-
-    # plt.figure(1)
-    #
-    # plt.subplot(211)
-    # plot_elliptic(-1,1)
-    # plt.grid()
-    #
-    # plt.subplot(212)
-    # plot_elliptic(-5,5)
-    # plt.grid()
-    # plt.show()
+    show_various_curves_comparison()
+    plt.show()
