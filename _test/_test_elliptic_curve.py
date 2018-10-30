b2995cb2dd5c04ebb2ffb373368a4951b2fc37a5
What Onito3 on Earth! <nitt0x0@gmail.com>
2018-10-31 01:51:19 +0900

    2.8-add ECCA checksum

--- _test/_test_elliptic_curve.py
+++ _test/_test_elliptic_curve.py
@@ -2,7 +2,7 @@
 # Various Elliptic Curve Ploting
 # Site: https://stackoverflow.com/questions/19756043/
 # python-matplotlib-elliptic-curves
-"""
+\n\n"""
 print(__doc__)
 
 import random
@@ -19,6 +19,21 @@ def plot_elliptic(a=-1, b=1):
         [0],
     )
     return _plot
+
+
+def plot_checksum(a, b):
+    a, b = np.ogrid[-5:5:100j, -5:5:100j]
+    _plot = plt.contour(
+        a.ravel(),
+        b.ravel(),
+        4 * pow(a, 3) + 27 * pow(b, 2),
+        [0],
+    )
+    return _plot
+
+
+def checksum(a, b):
+    return (4 * pow(a,3) + 47 * pow(b,2) == 0)
 
 
 def show_decorated(a, b, title=1, xlabel=1):
@@ -47,13 +62,13 @@ def show_random_2_curves():
 
 def show_step_b_step_curves():
     # change various a, b  check how graph changes
-    for _a in range(-5, 6, 1):
+    for _a in range(-12, 13, 1):
         # _a = [-5 ~ 5] / _b = 1
         plot_elliptic(_a, 1)
         show_decorated(_a, 1)
         plt.show()
 
-    for _b in range(-5, 6, 1):
+    for _b in range(-12, 13, 1):
         # _a = -1 / _b = [-5 ~ 5]
         plot_elliptic(1, _b)
         show_decorated(1, _b)
@@ -64,20 +79,35 @@ def show_various_curves_comparison():
     plt.figure(figsize=(15, 6))
 
     plt.subplot(121)
-    for _a in range(-5, 6, 1):
+    for _a in range(-12, 13, 1):
         # _a = [-5 ~ 5] / _b = 0
         plot_elliptic(_a, 0)
     show_decorated(_a, 0)
 
     plt.subplot(122)
-    for _b in range(-5, 6, 1):
+    for _b in range(-12, 13, 1):
         # _b = [-5 ~ 5] / _a = 0
         plot_elliptic(0, _b)
     show_decorated(0, _b)
 
+def show_plot_checksum():
+        for a in range(-12, 13, 1):
+            for b in range(-12, 13, 1):
+                if checksum(a, b):
+                    print("{2:}: 4*{0:}^3 + 27*{1:}^2 = 0 : [{0:},{1:}]".format(
+                                    a, b, checksum(a, b)
+                                )
+                            )
+                plot_checksum(a, b)
+        plt.grid()
+
+
 
 if __name__ == '__main__':
     # show_step_b_step_curves()
+
+    show_plot_checksum()
+    plt.show()
 
     show_random_2_curves()
     plt.show()
